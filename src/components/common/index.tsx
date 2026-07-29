606a5c7bc8f85d7a48bcab5e4cef60d89fa6ca84
Arslan Malik <arslanmalikgoraha@gmail.com>
2026-07-29 11:39:30 +0400

    fix(types): remove dead re-export of deleted LegacyDashboardPrimitives in common/index.tsx

--- src/components/common/index.tsx
+++ src/components/common/index.tsx
@@ -1,4 +1,4 @@
-﻿/**
+/**
  * Common components barrel export
  * Only exports components that are actively used in production code.
  * Dead exports (no production imports) have been removed.
@@ -7,19 +7,5 @@ export { default as PropertyCard, PropertyStatusBadge } from './PropertyCard';
 export { default as SubNavBar } from './SubNavBar';
 export { default as SuspenseLoader } from './SuspenseLoader';
 export { StatusProvider, useStatus } from './StatusNotification';
-export {
-	StatCard,
-	StatCardGrid,
-	TabbedPanel,
-	DataCard,
-	DataCardGrid,
-	DataList,
-	DataListItem,
-	QuickLinks,
-	ActionButton,
-	LeadListItem,
-	PropertyListItem,
-	PipelineBoard,
-	DealProgressBar,
-} from './LegacyDashboardPrimitives';
+
 
